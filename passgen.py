c2ee790a9c461352d12f192fab90ca40c0569702
shase <shase428@gmail.com>
2020-08-21 12:08:28 +0900

    fix type

--- passgen.py
+++ passgen.py
@@ -4,4 +4,4 @@ import string
 
 def passgenvim_generate_password(size=''):
    chars = string.ascii_letters + string.digits
-   print(''.join(secrets.choice(chars) for x in range(size)))
+   print(''.join(secrets.choice(chars) for x in range(int(size))))
